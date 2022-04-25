90150fc9d539a8150bcc7a62a945be454d946a10
viniciusrosner <viniciusrosner@gmail.com>
2022-04-25 17:22:53 -0300

    feature: smartphone responsiveness

--- js/owl/main.js
+++ js/owl/main.js
@@ -1,16 +1,29 @@
-$('.owl-carousel').owlCarousel({
+var owl = $('.owl-carousel');
+owl.owlCarousel({
     loop:true,
-    margin:10,
     nav:true,
+    margin:10,
     responsive:{
         0:{
             items:1
         },
         600:{
             items:3
-        },
-        1000:{
+        },            
+        960:{
             items:5
+        },
+        1200:{
+            items:6
         }
     }
-})
+});
+owl.on('mousewheel', '. owl-stage', function (e) {
+    if (e.deltaY>0) {
+        owl.trigger('prev.owl');
+    } else {
+        owl.trigger('next.owl');
+    }
+    e.preventDefault();
+});
+
